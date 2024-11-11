6f8a8f57ddea536a01c12def119bbd746eb9a0f5
ferchach_iliass <iliass.ferchach@gmail.com>
2024-11-11 11:48:26 +0100

    panier fonctionnel

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,17 +2,43 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class CartController extends AbstractController
 {
-    #[Route('/addToCart', name: 'cart.add')]
-    public function index(): Response
+    #[Route('/cart', name: 'cart.show')]
+    public function index(SessionInterface $session, ProductRepository $productRepository): Response
     {
+        $sessionCart = $session->get('panier',[]);
+        $cart = [];
+        foreach($sessionCart as $id => $quantity ){
+            $cart[] = ["product"=>$productRepository->find($id),"quantity"=>$quantity];
+        }
+
         return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+            'cart' => $cart
         ]);
+    }
+    
+    #[Route('/addToCart/{id}', name: 'cart.add')]
+    public function addToCart($id, SessionInterface $session): Response
+    {
+        $nb = isset($_POST['nb'])?$_POST['nb']:1;
+
+        $panier = $session->get('panier',[]);
+
+        if(empty($panier[$id])){
+            $panier[$id] = $nb;
+        }else{
+            $panier[$id] = $panier[$id] + $nb;
+        }
+
+        $session->set('panier', $panier);
+
+        return new Response();
     }
 }
